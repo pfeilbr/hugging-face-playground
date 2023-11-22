15308957d9e12845bb1bd6b42ae86bf69570be6a
Brian Pfeil <brian.pfeil@gmail.com>
2023-11-22 14:35:29 -0500

    added test_automatic_speech_recognition

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
 from transformers import pipeline, set_seed
-generator = pipeline('text-generation', model='gpt2')
-output = generator("Hello, I like to play cricket,", max_length=60, num_return_sequences=7)
-print(output)
+
+def test_text_generation():
+    generator = pipeline('text-generation', model='gpt2')
+    set_seed(42)
+    output = generator("Hello, I like to play cricket,", max_length=60, num_return_sequences=7)
+    print(output)
+
+
+def test_automatic_speech_recognition():
+    transcriber = pipeline(task="automatic-speech-recognition")
+    resp = transcriber("https://huggingface.co/datasets/Narsil/asr_dummy/resolve/main/mlk.flac")
+    print (resp)
+
+if __name__ == '__main__':
+    test_text_generation()
+    test_automatic_speech_recognition()
