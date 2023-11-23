98ee8a022af0153ec83db0c1d707d442a565512b
Brian Pfeil <brian.pfeil@gmail.com>
2023-11-22 20:32:07 -0500

    updates

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from transformers import pipeline, set_seed
+from transformers import pipeline, set_seed, AutoModelForCausalLM, AutoTokenizer
 
 def test_text_generation():
     generator = pipeline('text-generation', model='gpt2')
@@ -13,12 +13,23 @@ def test_automatic_speech_recognition():
     resp = transcriber("https://audio-samples.github.io/samples/mp3/blizzard_unconditional/sample-0.mp3")
     print (resp)
 
+# https://huggingface.co/docs/transformers/pipeline_tutorial
 def test_openai_whisper_speech_to_text():
     transcriber = pipeline(model="openai/whisper-large-v2", device_map="auto")
     resp = transcriber("https://audio-samples.github.io/samples/mp3/blizzard_unconditional/sample-0.mp3")
     print (resp)
 
+# https://huggingface.co/docs/transformers/llm_tutorial
+def test_local_llm_mistralai():
+    model = AutoModelForCausalLM.from_pretrained("mistralai/Mistral-7B-v0.1", device_map="auto", load_in_4bit=True)
+    tokenizer = AutoTokenizer.from_pretrained("mistralai/Mistral-7B-v0.1", padding_side="left")
+    model_inputs = tokenizer(["A list of colors: red, blue"], return_tensors="pt").to("cuda")
+    generated_ids = model.generate(**model_inputs)
+    resp = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
+    print (resp)
+
 if __name__ == '__main__':
-    #test_text_generation()
+    test_text_generation()
     #test_automatic_speech_recognition()
-    test_openai_whisper_speech_to_text()
+    #test_openai_whisper_speech_to_text()
+    #test_local_llm_mistralai()
